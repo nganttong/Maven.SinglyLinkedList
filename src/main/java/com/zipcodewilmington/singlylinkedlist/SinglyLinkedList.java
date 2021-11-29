0780d19a810bca8c0773fb18165a3f7ba51c00c1
cay <cay@zipcodes-MacBook-Pro-9.local>
2021-11-29 09:57:25 -0500

    added contains and find

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -24,13 +24,53 @@ public class SinglyLinkedList<T> implements Comparable {
     public void addNode(T info) {
         Node nodeToAdd = new Node(info);
 
-        if (head == null) {
-            head = nodeToAdd;
+        if (this.head == null) {
+            this.head = nodeToAdd;
         } else {
             tail.next = nodeToAdd;
         }
         tail = nodeToAdd;
     }
 
-    public void removeNode
+    public void removeNode(T itemToRemove) {
+        Node currentNode = this.head;
+        Node lastNode = this.head;
+
+        while(currentNode != null) {
+            if(currentNode.info.equals(itemToRemove)) {
+                if(currentNode.next == null) {
+                    lastNode.next = null;
+                } else if (currentNode == lastNode) {
+                    head = currentNode.next;
+                } else {
+                    lastNode.next = currentNode.next;
+                }
+            }
+            lastNode = currentNode;
+            currentNode = currentNode.next;
+        }
+    }
+
+    public boolean containsNode(T valueToContain) {
+        return this.find(valueToContain) >= 0;
+    }
+
+    private Integer find(T valueToFind) {
+        Node currentNode = this.head;
+        int index = 0;
+//        if(this.head == null){
+//            what do I do if the list is empty
+//        }
+        while(currentNode != null) {
+            if (currentNode.info == valueToFind) {
+                return index;
+            }
+            index++;
+            currentNode = currentNode.next;
+        }
+        return -1;
+    }
+
+
+
 }
