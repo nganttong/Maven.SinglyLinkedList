35e83ddd1f55643c82886a9e903209f654428420
cay <cay@zipcodes-MacBook-Pro-9.local>
2021-11-29 09:31:41 -0500

    setting up node

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -3,5 +3,22 @@ package com.zipcodewilmington.singlylinkedlist;
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> implements Comparable {
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    class Node{
+        T info;
+        Node next;
+
+        public Node(T info) {
+            this.info = info;
+            this.next = null;
+        }
+    }
+
+    public Node head = null;
+    public Node tail = null;
+
 }
